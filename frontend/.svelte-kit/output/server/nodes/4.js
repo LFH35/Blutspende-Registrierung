51f031e8df9eca817c1e989443cf5233cab91fbc
LFH35 <91213216+LFH35@users.noreply.github.com>
2023-12-30 00:02:39 +0100

    Delete frontend/.svelte-kit directory

--- frontend/.svelte-kit/output/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/question/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.ujTkdV4F.js","_app/immutable/chunks/scheduler.Smq_RA6i.js","_app/immutable/chunks/index.BamsqFJs.js"];
-export const stylesheets = ["_app/immutable/assets/4.dstgCF7H.css"];
-export const fonts = [];
