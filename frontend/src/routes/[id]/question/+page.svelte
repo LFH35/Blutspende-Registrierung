23ff592265e774d95c5ae7f8f4a4bde4c877d923
lfh35 <luca@halbich.net>
2024-01-03 14:34:27 +0100

    added js to questions

--- frontend/src/routes/[id]/question/+page.svelte
+++ frontend/src/routes/[id]/question/+page.svelte
@@ -10,32 +10,32 @@
     <div class="card">
         <b>Sind Sie im Alter zwischen 18 und 60 Jahre?</b>
         <div class="answer">
-            <button class="btn-ja">JA</button> <br>
-            <button class="btn-nein">NEIN</button>
+            <button class="btn-ja" on:click={() => nextQuestion('1')}>JA</button> <br>
+            <button class="btn-nein" on:click={() => nextQuestion('0')}>NEIN</button>
         </div>
     </div>
 
     <div class="card">
         <b>Wiegen Sie über 50kg?</b>
         <div class="answer">
-            <button class="btn-ja">JA</button> <br>
-            <button class="btn-nein">NEIN</button>
+            <button class="btn-ja" on:click={() => nextQuestion('1')}>JA</button> <br>
+            <button class="btn-nein" on:click={() => nextQuestion('0')}>NEIN</button>
         </div>
     </div>
 
     <div class="card">
         <b>Fühlen Sie sich gesund?</b>
         <div class="answer">
-            <button class="btn-ja">JA</button> <br>
-            <button class="btn-nein">NEIN</button>
+            <button class="btn-ja" on:click={() => nextQuestion('1')}>JA</button> <br>
+            <button class="btn-nein" on:click={() => nextQuestion('0')}>NEIN</button>
         </div>
     </div>
 
     <div class="card">
         <b>Wurde Ihnen in den letzten vier Monaten Tattoos, Piercings oder Ohrringe gestochen?</b>
         <div class="answer">
-            <button class="btn-ja">JA</button> <br>
-            <button class="btn-nein">NEIN</button>
+            <button class="btn-ja" on:click={() => nextQuestion('1')}>JA</button> <br>
+            <button class="btn-nein" on:click={() => nextQuestion('0')}>NEIN</button>
         </div>
     </div>
 
@@ -48,24 +48,30 @@
 {/if}-->
 
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        var questions = document.querySelectorAll('.card');
-        var index = 0;
-        var solution = "";
+    import { onMount } from "svelte";
 
-        questions[index].style.display = 'block';
-    });
+    let questions = [];
+    let solution = "";
+    let index = 0;
 
-    function nextQuestion(answer) {
-        solution += answer; 
+    onMount( () => {
+    questions = document.querySelectorAll('.card');
+
+    questions[index].style.display = 'block'; 
+});
+
+function nextQuestion(answer) {
+        solution += answer;
 
         questions[index].style.display = 'none';
 
-        if (index < questions.length - 1) {
-            index++; // Gehe zur nächsten Frage
-            questions[index].style.display = 'block'; // Zeige die nächste Frage an
+        index++; 
+
+        if (index < questions.length) { 
+            questions[index].style.display = 'block'; 
         } else {
-            document.getElementById('hinweisText').innerHTML = 'Denken Sie bitte an Ihren gültigen Personalausweis oder einen gültigen Reisepass! Vergessen Sie nicht vor und nach der Blutspende außreichend zu trinken!';
+            // document.getElementById('hinweisText').innerHTML = 'Vielen Dank für Ihre Teilnahme!';
         }
     }
+
 </script>
