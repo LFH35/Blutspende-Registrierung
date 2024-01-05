0c25400f258dd52c5f3fc4df8f59c38faf1ed424
lfh35 <luca@halbich.net>
2024-01-05 15:48:21 +0100

    added buchung Bestätigen

--- frontend/src/routes/[id]/appointments/[time]/+page.svelte
+++ frontend/src/routes/[id]/appointments/[time]/+page.svelte
@@ -1,1 +1,37 @@
-Hello world
+<style>
+    @import 'style.scss';
+</style>
+
+<h2>Buchung Bestätigen</h2>
+
+<div class="content">
+    <p>Sind sie sicher das sie den Termin um <b>{time}Uhr</b> buchen möchten? <br> <br> Aktuell sind noch <b>{freeSlots} Plätze</b> frei.</p>
+    <button on:click={() => terminBuchen()}>Termin Buchen</button>
+    <img src="../../favicon.png" alt="Bluttropfen mit Checkliste">
+</div>
+
+<script>
+    import { onMount } from "svelte";
+    import { goto } from '$app/navigation';
+
+        let thisPage = [];
+        let id;
+        let appointment= [];
+        let time;
+        let freeSlots;
+
+    onMount( () => {
+        thisPage = window.location.pathname.split('/');
+        id = thisPage[0];
+        appointment = thisPage[3].split('+');
+        time = appointment[0];
+        freeSlots = appointment[1];
+    });
+
+    function terminBuchen() {
+        console.log(time);
+        console.log(freeSlots);
+
+        //#TODO termin mit id und time buchen
+    }
+</script>
