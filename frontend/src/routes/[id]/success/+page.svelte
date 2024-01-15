460267298095f5eff161455f95e2ffe4aa32a5d9
lfh35 <luca@halbich.net>
2024-01-15 17:10:34 +0100

    added success design

--- frontend/src/routes/[id]/success/+page.svelte
+++ frontend/src/routes/[id]/success/+page.svelte
@@ -3,9 +3,8 @@
 </style>
 
 <div class="card">
-    <h2>Ihr Termin ist erfolgreich gebucht! <br> Sie haben von uns eine E-Mail zur Bestätigung des Termines bekommen. <br>
-        Sie werden den Tag vor dem Termin ein Formular von uns per E-Mail zugesendet bekommen, welches Sie schon einmal
-        vorab ausfüllen können um den Prozess vor Ort zu beschleunigen.</h2>
+    <h2>Ihr Termin ist erfolgreich gebucht! <br> <br> In kürze werden sie eine Bestätigung per E-Mail erhalten.</h2>
+        <p><b>Am Tag vor ihrer Blutspende erhalten sie per E-Mail ein Formular, dieses dürfen sie Bereits ausfüllen und am Tag ihrer Spende mitbringen um den Prozess vor Ort zu Beschleunigen.</b></p>
     <img src="../../favicon.png" alt="Bluttropfen mit Checkliste">
 </div>
 
