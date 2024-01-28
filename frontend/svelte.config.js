a6b654ef475615f9d94c760091e2ff1840ab903f
AmphoMoronal <amphomoronal@gmail.com>
2024-01-28 10:19:29 +0100

    removed cloudflare-adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,16 +1,6 @@
-import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 export default {
-	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
-	},
 	preprocess: preprocess({
 		scss: {
 			prependData: `@import './src/routes/global.scss';`
